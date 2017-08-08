0f0a160498f4f6521042761ae8b340584d9fd4c0
michael.geiss <michael.geiss@europace.de>
2017-08-08 08:16:00 +0200

    retrieve data from storage

--- src/itunes-update-check.js
+++ src/itunes-update-check.js
@@ -1,31 +1,39 @@
-// const request = require('request@2.81.0');
-const requestPromise = require('request-promise');
-module.exports = function (ctx, cb) {
-  const items = [
-    {"name": "FileBot", "id": 905384638, "price": 21.99},
-    {"name": "Tagr", "id": 903733783, "price": 9.99},
-    {"name": "Agricola", "id": 561521557, "price": 7.99},
-    {"name": "D&D Lords of Waterdeep", "id": 648019675, "price": 7.99},
-    {"name": "Affinity Photo", "id": 1117941080, "price": 21.99},
-    {"name": "Pixelmator", "id": 407963104, "price": 32.99}
-  ];
+'use strict';
 
-  var appInfoPromises = ladeAppInfos(items);
-  Promise.all(appInfoPromises).then(values => {
-    actualAppInfos = reduziereAktuelleAppInfos(values);
-    schreibeAktuelleUndErwarteteAppInfos(items, actualAppInfos);
+const requestPromise = require('request-promise');
+
+module.exports = function (context, callback) {
+
+  ladeDatenAusStorage().then((items) => {
+    let appInfoPromises = ladeAppInfos(items);
+    Promise.all(appInfoPromises).then(values => {
+      let actualAppInfos = reduziereAktuelleAppInfos(values);
+      schreibeAktuelleUndErwarteteAppInfos(items, actualAppInfos);
+    });
   });
-  y
+
+  function ladeDatenAusStorage() {
+    return new Promise((resolve, reject) => {
+      context.storage.get(function (error, data) {
+        if (error) {
+          reject(error)
+        }
+        else {
+          resolve(data)
+        }
+      });
+    });
+  }
 
   function reduziereAktuelleAppInfos(actualAppInfos) {
     return actualAppInfos.map(appInfo => {
-      var current = appInfo.results[0];
+      const current = appInfo.results[0];
       return {name: current.trackName, id: current.trackId, price: current.price};
     });
   }
 
   function schreibeAktuelleUndErwarteteAppInfos(currentAppInfo, actualAppInfos) {
-    cb(null, {
+    callback(null, {
       expected: currentAppInfo,
       current: actualAppInfos,
       lastCalled: new Date().toUTCString()
