8314d24f369c1acb829f68390db59dc77fff512c
michael.geiss <michael.geiss@europace.de>
2017-08-08 08:14:29 +0200

    alignment

--- src/itunes-update-check.js
+++ src/itunes-update-check.js
@@ -1,40 +1,41 @@
 // const request = require('request@2.81.0');
 const requestPromise = require('request-promise');
-module.exports = function(ctx, cb) {
-    const items = [
-      {"name": "FileBot", "id": 905384638, "price": 21.99},
-      {"name": "Tagr", "id": 903733783, "price": 9.99},
-      {"name": "Agricola", "id": 561521557, "price": 7.99},
-      {"name": "D&D Lords of Waterdeep", "id": 648019675, "price": 7.99},
-      {"name": "Affinity Photo", "id": 1117941080, "price": 21.99},
-      {"name": "Pixelmator", "id": 407963104, "price": 32.99}
-    ];
-    
-    var appInfoPromises = ladeAppInfos(items);
-    Promise.all(appInfoPromises).then(values => {
-       actualAppInfos = reduziereAktuelleAppInfos(values);
-       schreibeAktuelleUndErwarteteAppInfos(items, actualAppInfos);
+module.exports = function (ctx, cb) {
+  const items = [
+    {"name": "FileBot", "id": 905384638, "price": 21.99},
+    {"name": "Tagr", "id": 903733783, "price": 9.99},
+    {"name": "Agricola", "id": 561521557, "price": 7.99},
+    {"name": "D&D Lords of Waterdeep", "id": 648019675, "price": 7.99},
+    {"name": "Affinity Photo", "id": 1117941080, "price": 21.99},
+    {"name": "Pixelmator", "id": 407963104, "price": 32.99}
+  ];
+
+  var appInfoPromises = ladeAppInfos(items);
+  Promise.all(appInfoPromises).then(values => {
+    actualAppInfos = reduziereAktuelleAppInfos(values);
+    schreibeAktuelleUndErwarteteAppInfos(items, actualAppInfos);
+  });
+  y
+
+  function reduziereAktuelleAppInfos(actualAppInfos) {
+    return actualAppInfos.map(appInfo => {
+      var current = appInfo.results[0];
+      return {name: current.trackName, id: current.trackId, price: current.price};
     });
- 
-    function reduziereAktuelleAppInfos(actualAppInfos){
-       return actualAppInfos.map(appInfo => {
-          var current = appInfo.results[0];
-          return {name: current.trackName, id:current.trackId, price:current.price};
-       });
-    }
-    
-    function schreibeAktuelleUndErwarteteAppInfos(currentAppInfo, actualAppInfos){
-        cb(null, {
-            expected: currentAppInfo,
-            current: actualAppInfos,
-            lastCalled: new Date().toUTCString()
-        });
-    }
-    
-    function ladeAppInfos(items){
-      return items.map((item) => {
-        const url = "https://itunes.apple.com/de/lookup?id=" + item.id;
-        return requestPromise({url: url, json: true});
-      });
-    }
+  }
+
+  function schreibeAktuelleUndErwarteteAppInfos(currentAppInfo, actualAppInfos) {
+    cb(null, {
+      expected: currentAppInfo,
+      current: actualAppInfos,
+      lastCalled: new Date().toUTCString()
+    });
+  }
+
+  function ladeAppInfos(items) {
+    return items.map((item) => {
+      const url = "https://itunes.apple.com/de/lookup?id=" + item.id;
+      return requestPromise({url: url, json: true});
+    });
+  }
 };
--- src/preis-update-vergleichslogik.js
+++ src/preis-update-vergleichslogik.js
@@ -1,37 +1,38 @@
 const requestPromise = require('request-promise');
 const SlackBot = require('slackbots');
 
-module.exports = function(context, callback) {
-    var bot = new SlackBot({token:context.secrets.slackbotToken, name:context.secrets.slackbotName});
- 
-    ladeITunesDaten().then(result => {
-     vergleicheAppInfos(result.current, result.expected);
+module.exports = function (context, callback) {
+  var bot = new SlackBot({token: context.secrets.slackbotToken, name: context.secrets.slackbotName});
+
+  ladeITunesDaten().then(result => {
+    vergleicheAppInfos(result.current, result.expected);
+  });
+
+  function ladeITunesDaten() {
+    return requestPromise({url: context.secrets.itunesUpdateUrl, json: true});
+  }
+
+  function vergleicheAppInfos(actual, expected) {
+    actual.forEach(currentItem => {
+      var expectedItem = expected.find(x => x.id === currentItem.id);
+      if (!expected) {
+        var fehlerText = 'kein Vergleichobjekt';
+        poste(fehlerText);
+        throw Error(fehlerText);
+      }
+      if (expectedItem.price !== currentItem.price) {
+        var text = "Preis von " + currentItem.name + " hat sich geändert: " + expectedItem.price + " -> " + currentItem.price;
+        poste(text);
+        console.log(text);
+        callback(null, {"aenderungen": true, lastCalled: new Date().toUTCString()});
+      }
+      else {
+        callback(null, {"aenderungen": false, lastCalled: new Date().toUTCString()});
+      }
     });
-    
-    function ladeITunesDaten(){
-       return requestPromise({url: context.secrets.itunesUpdateUrl, json: true});
-    }
-    
-    function vergleicheAppInfos(actual, expected){
-       actual.forEach(currentItem => {
-        var expectedItem = expected.find(x => x.id === currentItem.id);
-        if(!expected){
-          var fehlerText = 'kein Vergleichobjekt';
-          poste(fehlerText);
-          throw Error(fehlerText);
-        }
-        if(expectedItem.price !== currentItem.price){
-          var text = "Preis von " + currentItem.name + " hat sich geändert: " + expectedItem.price + " -> " + currentItem.price; 
-          poste(text);
-          console.log(text);
-          callback(null,{"aenderungen":true, lastCalled: new Date().toUTCString()});
-        } else {
-          callback(null,{"aenderungen":false, lastCalled: new Date().toUTCString()});
-        }
-      });
-    }
-    
-    function poste(text){
-      bot.postMessageToChannel(context.secrets.slackbotChannelName, text,  {icon_emoji : ":android:"}, null);
-    }
+  }
+
+  function poste(text) {
+    bot.postMessageToChannel(context.secrets.slackbotChannelName, text, {icon_emoji: ":android:"}, null);
+  }
 };
